5c74e97dad4fbd50eaa53e0789af6dbde66d28db
Rohan Puranik <59736919+Rohan-3@users.noreply.github.com>
2024-01-09 12:56:42 +0530

    Folder Structure Created!

---
+++ fithub/src/components/ResgistrationForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ResgistrationForm = () => {
+  return (
+    <div>ResgistrationForm</div>
+  )
+}
+
+export default ResgistrationForm
---
+++ fithub/src/pages/Registration.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ResgistrationForm from '../components/ResgistrationForm'
+import Navbar from '../components/Navbar'
+
+const Registration = () => {
+  return (
+    <div>
+    <Navbar/>
+        <ResgistrationForm/>
+        <footer/>
+    </div>
+  )
+}
+
+export default Registration
