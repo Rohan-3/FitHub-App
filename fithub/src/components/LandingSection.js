319d829c573759d44ad11671ed831a3b8f574fd8
Rohan Puranik <59736919+Rohan-3@users.noreply.github.com>
2024-01-11 02:10:38 +0530

    HomePage Landing Section

---
+++ fithub/src/components/LandingSection.js
@@ -0,0 +1,26 @@
+import {React} from 'react'
+import '../assets/styles/BgVideo.css'
+import {useTypewriter} from 'react-simple-typewriter'
+import BgVideo from '../assets/videos/BgVideo.mp4'
+
+const LandingSection = () => {
+  const [typeEffect] = useTypewriter({
+    words:['FitHub !', 'fitHub !!'],
+    loop:{},
+    typeSpeed: 200,
+    delaySpeed:130
+  })
+  return (
+
+    <div className='landing'>
+        <video src={BgVideo} autoPlay muted loop />
+        <div className='content'>
+            <h1>Welcome to <span>{typeEffect}</span></h1>
+            <p className='intro-para'>"Step into a world of fitness and wellness. Your transformation begins now!"</p>
+        </div>
+    </div>
+    
+  )
+}
+
+export default LandingSection
--- fithub/src/pages/Auth.js
+++ fithub/src/pages/Auth.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import ResgistrationForm from '../components/ResgistrationForm'
 import Login from '../components/Login';
+import '../assets/styles/Auth.css'
 const Auth = () => {
     const [flag, setFlag] = useState();
   return (
     <div>
+    <div className='toogleBtns'>
     <button onClick={()=> setFlag(true)}>Register</button>
     <button onClick={()=> setFlag(false)}>Login</button>
+    </div>
     {flag === true? <ResgistrationForm/> : flag === false? <Login/> : <ResgistrationForm/>} 
     </div>
   )
