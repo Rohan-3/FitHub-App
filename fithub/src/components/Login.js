098fa9c56bfd9779740b9b96f2cc8a86b06607db
K-Sruti <ksruti332@gmail.com>
2024-01-12 15:12:02 +0530

    Change the authentication design in Registration page

--- fithub/src/components/Login.js
+++ fithub/src/components/Login.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <div>
+      <h1>Login with your Phone Number</h1>
+      <input type="text" placeholder='Enter your phone number'/>
+      <button>Send OTP</button>
+      </div>
+      <div>
+        <h1>Verify OTP</h1>
+        <input type="text" placeholder="Enter OTP" />
+        <button>Verify OTP</button>
+      </div>
+    </div>
   )
 }
 
--- fithub/src/pages/Auth.js
+++ fithub/src/pages/Auth.js
@@ -2,15 +2,25 @@ import React, { useState } from 'react'
 import ResgistrationForm from '../components/ResgistrationForm'
 import Login from '../components/Login';
 import '../assets/styles/Auth.css'
+import AuthImage from '../assets/images/AuthImage.jpeg'
 const Auth = () => {
     const [flag, setFlag] = useState();
   return (
     <div>
+    <div className='wholePage'>
+    <div className='leftSide'>
+      <img src={AuthImage} alt='image' className='Aimage'/>
+    </div>
+    <div className='rightSide'>
     <div className='toogleBtns'>
     <button onClick={()=> setFlag(true)}>Register</button>
     <button onClick={()=> setFlag(false)}>Login</button>
     </div>
-    {flag === true? <ResgistrationForm/> : flag === false? <Login/> : <ResgistrationForm/>} 
+    <div>
+    {flag === true? <ResgistrationForm/> : flag === false? <Login/> : <ResgistrationForm/>}
+    </div>
+    </div>
+    </div>
     </div>
   )
 }
